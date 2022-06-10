51ac11f28ad65f979ba39bf48a23ff135c89ee12
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-06-10 15:08:21 +0100

    Add note component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Note from "./Note";
 
 function App() {
   return (
     <div>
       <Header />
+      <Note />
     </div>
   );
 }
